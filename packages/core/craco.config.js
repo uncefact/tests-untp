6c2d83f2f41ed972260c81699573b2fcfd118238
Yen Le <ldhyen99@gmail.com>
2024-01-23 16:58:31 +0700

    chore: update craco config in core packages

--- packages/core/craco.config.js
+++ packages/core/craco.config.js
@@ -1,18 +1,8 @@
 const webpack = require('webpack')
-const path = require('path')
-
-const ouputDir =
-  process.env.REACT_APP_MODE === 'verifier'
-    ? 'build/verifier'
-    : 'build/explorer'
 
 module.exports = {
-  babel: {
-    plugins: ['@babel/plugin-syntax-import-assertions'],
-  },
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
-      paths.appBuild = webpackConfig.output.path = path.resolve(ouputDir)
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
         fs: false,
