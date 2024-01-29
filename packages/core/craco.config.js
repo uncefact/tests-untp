30d2dae0fa13166b524b3c0a6e311cd0fa76a20a
Yen Le Diep Hoang <59736798+ldhyen99@users.noreply.github.com>
2024-01-29 10:30:06 +0700

    chore: set up babel core package (#6)
    * chore: add babel config in core package
    
    * refactor: delete file unused in core package

--- packages/core/craco.config.js
+++ packages/core/craco.config.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 module.exports = {
   webpack: {
@@ -6,9 +6,16 @@ module.exports = {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
         fs: false,
-      }
+      };
       /* ... */
-      return webpackConfig
+      return webpackConfig;
+    },
+    babel: {
+      presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-typescript'],
+      plugins: ['@babel/plugin-syntax-import-assertions'],
+      loaderOptions: (babelLoaderOptions) => {
+        return babelLoaderOptions;
+      },
     },
     plugins: [
       new webpack.ProvidePlugin({
@@ -16,4 +23,4 @@ module.exports = {
       }),
     ],
   },
-}
+};
