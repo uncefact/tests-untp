a237c754be90cbdd8d6e6346494803ea1b6c3e81
Yen Le <ldhyen99@gmail.com>
2024-01-29 14:49:30 +0700

    refactor: change type in issue event similar to integrate vckit function

--- packages/services/src/types/types.ts
+++
@@ -1,8 +0,0 @@
-import { CredentialPayload } from '@vckit/core-types';
-
-export interface IssueEvent {
-  credentialPayload: CredentialPayload;
-  credentialSubject: any;
-  restOfVC: any;
-  context: string[];
-}
