33c47f71b6f9eba90ff5646d6d3f66d096102097
Yen Le <ldhyen99@gmail.com>
2024-01-29 13:43:33 +0700

    feat: add base event service

---
+++ packages/services/src/models/vckit.ts
@@ -0,0 +1,9 @@
+export const contextDefault = [
+    'https://www.w3.org/2018/credentials/v1',
+    'https://w3id.org/vc-revocation-list-2020/v1',
+    'https://w3id.org/security/suites/jws-2020/v1',
+    'https://dev-render-method-context.s3.ap-southeast-1.amazonaws.com/dev-render-method-context.json',
+    'https://w3id.org/security/suites/jws-2020/v1',
+];
+
+export const typeDefault = ['VerifiableCredential'];
