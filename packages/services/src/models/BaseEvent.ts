a840f5383d5d1e33db89467feea70090928ed2e5
Yen Le <ldhyen99@gmail.com>
2024-01-30 09:21:16 +0700

    docs: add comment in base event class and document in readme

--- packages/services/src/models/BaseEvent.ts
+++ packages/services/src/models/BaseEvent.ts
@@ -1,6 +1,11 @@
 import { VerifiableCredential } from '@vckit/core-types';
 import { IVcKitIssueVC, integrateVckitIssueVC } from '../vckit.service';
 
+/**
+ * @description BaseEvent class is the base class for the events extending the class
+ * @param template - template for the event
+ * @param schema - schema for the event
+ */
 export abstract class BaseEvent {
   private template?: any;
   private schema?: any;
@@ -10,6 +15,10 @@ export abstract class BaseEvent {
     this.schema = schema;
   }
 
+  /**
+   * @description createEvent method is used to create the event
+   * @param arg - arguments for the event
+   */
   public createEvent(arg) {
     const { eventType } = arg;
     switch (eventType) {
@@ -26,13 +35,18 @@ export abstract class BaseEvent {
     }
   }
 
+  /**
+   * @description issueVC method is used to issue the VC
+   * @param arg - arguments for the VC
+   */
   async issueVC(arg: IVcKitIssueVC): Promise<VerifiableCredential> {
     try {
-      const { credentialPayload, credentialSubject, restOfVC, context, vcKitAPIUrl } = arg;
+      const { credentialPayload, credentialSubject, issuer, restOfVC, context, vcKitAPIUrl } = arg;
       const credentialValue: VerifiableCredential = await integrateVckitIssueVC({
         context,
         credentialSubject,
         ...credentialPayload,
+        issuer,
         restOfVC,
         vcKitAPIUrl,
       });
