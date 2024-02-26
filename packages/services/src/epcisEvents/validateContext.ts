6c522f9319725b933c7abcf25f9d0c6a66e8aef6
Yen Le <ldhyen99@gmail.com>
2024-02-26 17:11:54 +0700

    refactor: change code in validate context

--- packages/services/src/epcisEvents/validateContext.ts
+++ packages/services/src/epcisEvents/validateContext.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import { Result } from '../types/validateContext';
-import { IConfigDLR, IContext, IEntityIssue, IStorageContext, IVCKitContext } from './types';
+import { IContext } from './types';
 
 export const error: <T>(message: string) => Result<T> = (message) => ({
   ok: false,
@@ -17,72 +17,35 @@ export const checkContextProperties = (context: IContext): Result<IContext> => {
   return { ok: true, value: context };
 };
 
-export const validateVckit = (vckit: any): Result<IVCKitContext> => {
-  if (_.isEmpty(vckit.vckitAPIUrl)) return error('Invalid vckitAPIUrl');
-  if (_.isEmpty(vckit.issuer)) return error('Invalid issuer');
-
-  return { ok: true, value: vckit };
-};
-
-export const validateDpp = (dpp: any): Result<IEntityIssue> => {
-  if (_.isEmpty(dpp.context)) return error('Invalid context');
-  if (_.isEmpty(dpp.type)) return error('Invalid type');
-  if (_.isEmpty(dpp.dlrLinkTitle)) return error('Invalid dlrLinkTitle');
-  if (_.isEmpty(dpp.dlrVerificationPage)) return error('Invalid dlrVerificationPage');
-  if (_.isEmpty(dpp.dlrIdentificationKeyType)) return error('Invalid dlrIdentificationKeyType');
-
-  return { ok: true, value: dpp };
-};
-
-export const validateStorage = (storage: any): Result<IStorageContext> => {
-  if (_.isEmpty(storage.storageAPIUrl)) return error('Invalid storageAPIUrl');
-  if (_.isEmpty(storage.bucket)) return error('Invalid bucket');
-
-  return { ok: true, value: storage };
-};
-
-export const validateDlr = (dlr: any): Result<IConfigDLR> => {
-  if (_.isEmpty(dlr.dlrAPIUrl)) return error('Invalid dlrAPIUrl');
-  if (_.isEmpty(dlr.dlrAPIKey)) return error('Invalid dlrAPIKey');
-
-  return { ok: true, value: dlr };
-};
-
-export const validateIdentifierKeyPaths = (identifierKeyPaths: string[]): Result<string[]> => {
-  if (identifierKeyPaths.length === 0) return error('Invalid identifierKeyPaths');
-
-  return { ok: true, value: identifierKeyPaths };
-};
-
 /**
  * Validate context properties
  * @param context - event context
  * @returns Result<IContext>
  */
-export const validateContext = (context: IContext): Result<IContext> => {
+export const validateContextObjectEvent = (context: IContext): Result<IContext> => {
   try {
     const validationResult = checkContextProperties(context);
     if (!validationResult.ok) return error(validationResult.message);
 
-    const vckitValidationResult = validateVckit(context.vckit);
-    if (!vckitValidationResult.ok) return error(vckitValidationResult.message);
+    if (_.isEmpty(context.vckit.vckitAPIUrl)) return error('Invalid vckitAPIUrl');
+    if (_.isEmpty(context.vckit.issuer)) return error('Invalid issuer');
 
-    const dppValidationResult = validateDpp(context.dpp);
-    if (!dppValidationResult.ok) return error(dppValidationResult.message);
+    if (_.isEmpty(context.dpp.context)) return error('Invalid context');
+    if (_.isEmpty(context.dpp.type)) return error('Invalid type');
+    if (_.isEmpty(context.dpp.dlrLinkTitle)) return error('Invalid dlrLinkTitle');
+    if (_.isEmpty(context.dpp.dlrVerificationPage)) return error('Invalid dlrVerificationPage');
+    if (_.isEmpty(context.dpp.dlrIdentificationKeyType)) return error('Invalid dlrIdentificationKeyType');
 
-    const storageValidationResult = validateStorage(context.storage);
-    if (!storageValidationResult.ok) return error(storageValidationResult.message);
+    if (_.isEmpty(context.storage.storageAPIUrl)) return error('Invalid storageAPIUrl');
+    if (_.isEmpty(context.storage.bucket)) return error('Invalid bucket');
 
-    const dlrValidationResult = validateDlr(context.dlr);
-    if (!dlrValidationResult.ok) return error(dlrValidationResult.message);
+    if (_.isEmpty(context.dlr.dlrAPIUrl)) return error('Invalid dlrAPIUrl');
+    if (_.isEmpty(context.dlr.dlrAPIKey)) return error('Invalid dlrAPIKey');
 
-    const identifierKeyPathsValidationResult = validateIdentifierKeyPaths(context.identifierKeyPaths);
-    if (!identifierKeyPathsValidationResult.ok) return error(identifierKeyPathsValidationResult.message);
+    if (context.identifierKeyPaths.length === 0) return error('Invalid identifierKeyPaths');
 
     return { ok: true, value: context };
   } catch (error) {
     throw new Error(error.message ?? 'Error validating context');
   }
 };
-
-export const validateContextObjectEvent = validateContext;
