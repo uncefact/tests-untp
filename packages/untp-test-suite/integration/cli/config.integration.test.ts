79be1323c9405d9e03d59613d9e8ef4bfd01dd96
Yen Le <ldhyen99@gmail.com>
2024-03-25 14:30:36 +0700

    refactor: change name of test cases in config integration test

--- packages/untp-test-suite/integration/cli/config.integration.test.ts
+++ packages/untp-test-suite/integration/cli/config.integration.test.ts
@@ -1,97 +1,61 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
-describe('yarn untp Options', () => {
-  it('should returns the untp help by default', () => {
-    exec('yarn untp', (error, stdout) => {
+describe("CLI 'untp' Options", () => {
+  const execCommand = (command: string, expectedOutput: string) => {
+    exec(command, (error, stdout) => {
       if (error) {
         console.error(`execSync error: ${error}`);
         return;
       }
 
       expect(stdout).not.toBeNull();
-      expect(stdout).toContain('Usage: untp [options] [command]');
+      expect(stdout).toContain(expectedOutput);
+    });
+  };
+
+  describe('with yarn', () => {
+    it('should display the untp help by default', () => {
+      execCommand('yarn untp', 'Usage: untp [options] [command]');
+    });
+
+    it('should display the untp help with --help', () => {
+      execCommand('yarn untp --help', 'Usage: untp [options] [command]');
+    });
+
+    it('should return the untp version', () => {
+      execCommand('yarn untp --version', '0.0.1');
     });
   });
 
-  it('should returns the untp help with --help', () => {
-    exec('yarn untp --help', (error, stdout) => {
-      if (error) {
-        console.error(`execSync error: ${error}`);
-        return;
-      }
-
-      expect(stdout).not.toBeNull();
-      expect(stdout).toContain('Usage: untp [options] [command]');
+  describe('with npm', () => {
+    it('should display the untp help by default', () => {
+      execCommand('npm run untp', 'Usage: untp [options] [command]');
     });
-  });
 
-  it('should returns the untp version', () => {
-    exec('yarn untp --version', (error, stdout) => {
-      if (error) {
-        console.error(`execSync error: ${error}`);
-        return;
-      }
-
-      expect(stdout).not.toBeNull();
-      expect(stdout).toContain('0.0.1');
+    it('should display the untp help with --help', () => {
+      execCommand('npm run untp -- --help', 'Usage: untp [options] [command]');
     });
-  });
-});
 
-describe('yarn untp Commands', () => {
-  describe('config Command', () => {
-    it('should create credential file successful', () => {
-      const credentialFileName = 'credentials.json';
-      const storePath = `${process.cwd()}/${credentialFileName}`;
-
-      exec('yarn untp config', (error, stdout) => {
-        if (error) {
-          console.error(`execSync error: ${error}`);
-          return;
-        }
-
-        expect(stdout).not.toBeNull();
-        expect(fs.existsSync(storePath)).toBe(true);
-      });
+    it('should return the untp version', () => {
+      execCommand('npm run untp -- --version', '0.0.1');
     });
   });
 });
 
-describe('npm untp Options', () => {
-  it('should returns the untp help by default', () => {
-    exec('npm run untp', (error, stdout) => {
+describe("CLI 'untp config' Commands", () => {
+  it('should create a credential file successfully', () => {
+    const credentialFileName = 'credentials.json';
+    const storePath = `${process.cwd()}/${credentialFileName}`;
+
+    exec('yarn untp config', (error, stdout) => {
       if (error) {
         console.error(`execSync error: ${error}`);
         return;
       }
 
       expect(stdout).not.toBeNull();
-      expect(stdout).toContain('Usage: untp [options] [command]');
-    });
-  });
-
-  it('should returns the untp help with -h', () => {
-    exec('npm run untp -- -h', (error, stdout) => {
-      if (error) {
-        console.error(`execSync error: ${error}`);
-        return;
-      }
-
-      expect(stdout).not.toBeNull();
-      expect(stdout).toContain('Usage: untp [options] [command]');
-    });
-  });
-
-  it('should returns the untp version', () => {
-    exec('npm run untp -- -v', (error, stdout) => {
-      if (error) {
-        console.error(`execSync error: ${error}`);
-        return;
-      }
-
-      expect(stdout).not.toBeNull();
-      expect(stdout).toContain('0.0.1');
+      expect(fs.existsSync(storePath)).toBe(true);
     });
   });
 });
