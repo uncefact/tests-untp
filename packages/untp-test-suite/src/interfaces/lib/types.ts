84a982ca42e82176c26f1db575de0ab54896e800
Huy Nguyen <huy.nguyenkim.h@gmail.com>
2024-03-27 11:34:52 +0700

    feat: add testMultiCredentialHandler and testCredentialHandler library functions

--- packages/untp-test-suite/src/interfaces/lib/types.ts
+++ packages/untp-test-suite/src/interfaces/lib/types.ts
@@ -1,5 +1,25 @@
-import { TestSuiteResult } from '../../core/types/index.js';
+import { ErrorObject } from 'ajv';
+import { ConfigCredentials, FinalStatus, TestSuiteResult } from '../../core/types/index.js';
 
-export interface TestSuiteHandler {
-  (credentialPath: string): Promise<TestSuiteResult>;
+export interface ITestCredentialHandlerResult {
+  credentialType?: string,
+  version?: string,
+  errors?: ErrorObject[] | null,
+  result: FinalStatus
 }
+
+export interface TestMultiCredentialHandler {
+  // This library function is intended for the test suite function to read the `credentials.json` file by the `credentialPath` parameter and run the entire test suite.
+  (credentialPath: string): Promise<TestSuiteResult>;
+
+  // This library function is used to pass the configuration credentials directly into the parameter and then run the test suite with those parameters.
+  (credential: ConfigCredentials): Promise<TestSuiteResult>;
+};
+
+export interface TestCredentialHandler {
+  // This library function is used to test a credential that specifies the credential type, version, and the test data that the user wants to test.
+  (credentialType: string, credentialVersion: string, testData: object): Promise<ITestCredentialHandlerResult>;
+
+  // This library function is used when a user wants to pass the schema directly to test with the provided test data.
+  (schema: object, testData: object): Promise<ITestCredentialHandlerResult>;
+};
