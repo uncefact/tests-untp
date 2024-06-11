290e6142ced69e42bcc67395d48bdd3255e16b0e
Yen Le Diep Hoang <59736798+ldhyen99@users.noreply.github.com>
2024-06-11 14:00:33 +0700

    feat: enhance uiux mock app (#53)
    * feat: add loading and show toast in button
    
    * feat: add tooltip on button
    
    * refactor: add tooltip and loading button when submit
    
    * refactor: change type in button

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -1,14 +1,34 @@
-import { Button } from '@mui/material';
+import { useState } from 'react';
+import { LoadingButton } from '@mui/lab';
+import { Tooltip } from '@mui/material';
 
 type ButtonProps = {
   onClick: () => void;
   label?: string;
+  description?: string;
 };
 
 export const CustomButton: React.FC<ButtonProps> = ({ onClick, label = 'Submit', ...props }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleOnClick = async () => {
+    setLoading(true);
+
+    try {
+      await onClick();
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
   return (
-    <Button onClick={onClick} {...props}>
-      {label}
-    </Button>
+    <div {...props}>
+      <Tooltip title={props.description}>
+        <LoadingButton loading={loading} variant='contained' onClick={handleOnClick}>
+          {label}
+        </LoadingButton>
+      </Tooltip>
+    </div>
   );
 };
