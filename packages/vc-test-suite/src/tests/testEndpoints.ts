c7b4bf6df68097d4f9e98935fd7825459f7d9167
Yen Le <ldhyen99@gmail.com>
2024-07-01 16:57:01 +0700

    feat: create test cases for QR Link Encryption Test Suite

--- packages/vc-test-suite/src/tests/testEndpoints.ts
+++ packages/vc-test-suite/src/tests/testEndpoints.ts
@@ -1,18 +1,23 @@
 import http from 'http';
+import https from 'https';
 import receiveJson from './receive-json.js';
 
 export async function post(endpoint: string, object: any): Promise<any> {
   const postData = Buffer.from(JSON.stringify(object));
 
   const res = await new Promise<http.IncomingMessage>((resolve, reject) => {
-    const req = http.request(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': postData.length,
-        Accept: 'application/json'
-      }
-    }, resolve);
+    const req = http.request(
+      endpoint,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': postData.length,
+          Accept: 'application/json',
+        },
+      },
+      resolve,
+    );
     req.on('error', reject);
     req.end(postData);
   });
@@ -20,6 +25,40 @@ export async function post(endpoint: string, object: any): Promise<any> {
   const result = await receiveJson(res);
   return {
     ...result,
-    statusCode: res.statusCode
+    statusCode: res.statusCode,
+  };
+}
+
+export async function get(url: any) {
+  try {
+    const res: any = await new Promise((resolve, reject) => {
+      const req = https.request(
+        url,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        },
+        resolve,
+      );
+      req.on('error', reject);
+      req.end();
+    });
+    const result = await receiveJson(res);
+    if (res.statusCode >= 400) {
+      if (result != null && result.errors) {
+        throw new Error(result.errors);
+      }
+      throw new Error(result);
+    }
+    if (res.statusCode >= 300) {
+      throw new Error('Redirect not supported');
+    }
+    return result;
+  } catch (e) {
+    const error = e as Error;
+    throw new Error(error.message);
   }
 }
