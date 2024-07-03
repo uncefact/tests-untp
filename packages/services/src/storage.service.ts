bfe927b5d7291a6ccb65fe6d0442d5dd1c3c2ec7
Duc Minh Pham <ducpm0511@gmail.com>
2024-07-03 17:05:54 +0700

    feat: removed console log

--- packages/services/src/storage.service.ts
+++ packages/services/src/storage.service.ts
@@ -18,7 +18,6 @@ export const storageService: IStorageService = async (config) => {
     }
 
     const getResultByPath = getValueByPath(result, params.resultPath);
-    console.log('getResultByPath', getResultByPath);
     return getResultByPath;
   } catch (error: any) {
     console.error(error);
