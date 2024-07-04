bfe4f21e4c4b2a0220e0f29cbd1e3335d168a251
Nam Hoang <hoangxuannam160493@gmail.com>
2024-07-04 11:16:04 +0700

    feat: set up test suite (#76)

---
+++ packages/vc-test-suite/tests/test-example.test.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { reportRow, setupMatrix } from '../helpers';
+
+describe('Matrix Test', function () {
+  setupMatrix.call(this, 'foo', 'Implementer');
+  reportRow('should be a cell in the matrix', 'foo', async function () {});
+  reportRow('should be a cell in the matrix1', 'foo', async function () {});
+  reportRow('should be a cell in the matrix2', 'foo', async function () {});
+});
