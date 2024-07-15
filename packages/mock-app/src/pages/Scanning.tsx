f8f7e2d7159578164cc7702bd71fee7cb8d7411f
Huy Nguyen <huy.nguyenkim.h@gmail.com>
2024-07-15 09:05:59 +0700

    fix: unable to scan the long barcodes on mobile devices

--- packages/mock-app/src/pages/Scanning.tsx
+++ packages/mock-app/src/pages/Scanning.tsx
@@ -92,8 +92,10 @@ const Scanning = () => {
       <Scanner
         ref={scannerRef}
         fps={30}
-        qrbox={{ width: 250, height: 150 }}
+        qrbox={{ width: 500, height: 300 }}
         disableFlip={false}
+        useBarCodeDetectorIfSupported={true}
+        focusMode= 'continuous'
         qrCodeSuccessCallback={onScanResult}
         qrCodeErrorCallback={onScanError}
       />
