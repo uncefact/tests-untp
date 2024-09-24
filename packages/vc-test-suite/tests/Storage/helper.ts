f8831294a7043b297f64a15ad123630411edc112
Ashley Harwood <60303491+ashleythedeveloper@users.noreply.github.com>
2024-09-24 14:13:40 +1000

    chore: update storage service tests (#108)

--- packages/vc-test-suite/tests/Storage/helper.ts
+++ packages/vc-test-suite/tests/Storage/helper.ts
@@ -21,15 +21,14 @@ interface EncryptedData {
  * @param {string} key - Decryption key in hexadecimal format
  * @returns {string} - Decrypted data
  */
-export function decryptData({ cipherText, iv, tag }: EncryptedData, key: string): string {
+export function decryptData({ cipherText, iv, tag }: EncryptedData, key: string): Record<string, any> | string {
   const decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(key, 'hex'), Buffer.from(iv, 'base64'));
   decipher.setAuthTag(Buffer.from(tag, 'base64'));
   let decrypted = decipher.update(cipherText, 'base64', 'utf8');
   decrypted += decipher.final('utf8');
 
-  // Try to decode the base64 string, if it fails, return the original string
   try {
-    return Buffer.from(decrypted, 'base64').toString('utf8');
+    return JSON.parse(decrypted);
   } catch (error) {
     return decrypted;
   }
