3acbcd1dfbb1506bd6e2073d23d83f0b23c14b6e
Nam Hoang <hoangxuannam160493@gmail.com>
2024-10-24 15:40:31 +0700

    chore: configure jest for lerna
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>
    (cherry picked from commit ee2f61c7b9651e64f5512b5248acfb3ba616f8b1)

--- packages/vc-test-suite/jest.config.js
+++ packages/vc-test-suite/jest.config.js
@@ -1,4 +1,7 @@
+const base = require('../../jest.config.base');
 module.exports = {
+  ...base,
+  collectCoverageFrom: ['<rootDir>/tests/**/*.{ts,tsx}', '!**/*.d.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>'],
