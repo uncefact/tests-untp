88931e092adba6e3e53d5ba06e840ac2f54dd804
Nam Hoang <hoangxuannam160493@gmail.com>
2024-10-28 10:26:11 +0700

    feat: implement traceability event

---
+++ packages/services/src/epcisEvents/traceabilityEvent.ts
@@ -0,0 +1,6 @@
+import { ITraceabilityEvent } from '../types';
+import { IService } from '../types/IService.js';
+export const processTraceabilityEvent: IService = async (
+  traceabilityEvent: ITraceabilityEvent,
+  context: any,
+): Promise<any> => {};
