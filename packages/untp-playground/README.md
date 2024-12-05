7cf4464c035a74cff1088afc2d84e1ebfc851aa2
Yen Le Diep Hoang <59736798+ldhyen99@users.noreply.github.com>
2024-12-05 10:58:20 +0700

    test: setup unit test for playground package (#174)
    * test: setup unit test for playground package
    
    * test: factor test strcuture in playground
    
    * chore: add modulePathIgnorePatterns to fix jest-haste-map collision in playground
    
    * chore: update jest config in playground to export json summary
    
    * doc: add document to run unit test in playground
    
    * chore: update test coverage from in playground

--- packages/untp-playground/README.md
+++ packages/untp-playground/README.md
@@ -11,6 +11,7 @@ Open [http://localhost:3000/](http://localhost:3000/) with your browser to see t
 ## Deployment
 
 We use Pulumi and GitHub actions to deploy the app. The following env variables needs to be set to define basePath in order to it work with the existing CloudFront Distribution:
+
 ```
 NEXT_PUBLIC_BASE_PATH: /untp-playground
 NEXT_PUBLIC_ASSET_PREFIX: /untp-playground
@@ -19,7 +20,7 @@ NEXT_PUBLIC_IMAGE_PATH: /untp-playground/_next/image
 
 GitHub OIDC is configured in AWS account for GitHub Actions workflow to assume and run the deployment using Pulumi. The role to assume is set as a repository secret `UNCEFACT_AWS_ROLE_TO_ASSUME`.
 Pulume security prodiver for config is set to awskms.
-The same backend state bucket and kms key are used for both test and prod Pulumi stacks.  
+The same backend state bucket and kms key are used for both test and prod Pulumi stacks.
 
 End-points:
 
@@ -30,3 +31,25 @@ End-points:
 In future production enddpoint will be replaced with a production url, and current endpoint will become test.
 
 The production build is configured using Docker image https://nextjs.org/docs/pages/building-your-application/deploying#docker-image
+
+## Testing
+
+We use Jest for testing.
+
+To run tests:
+
+```bash
+yarn test
+```
+
+To run tests in watch mode:
+
+```bash
+yarn test:watch
+```
+
+To generate coverage report:
+
+```bash
+yarn test:coverage
+```
--- packages/untp-playground/jest.config.js
+++ packages/untp-playground/jest.config.js
@@ -1,38 +1,28 @@
-const { defaults } = require('jest-config');
-const base = require('../../jest.config.base.js');
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
 
-module.exports = {
-  ...base,
-  rootDir: './',
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
-  collectCoverage: false,
-  collectCoverageFrom: [
-    '!**/infra/**',
-    '!**/.next/**',
-    '!**/types/**',
-    '!**/build/**',
-    '!**/node_modules/**',
-    '!**/**/index.ts',
-  ],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+const config = {
+  clearMocks: true,
   coverageReporters: ['text', 'lcov', 'json', 'json-summary'],
-  coverageProvider: 'v8',
-  coverageDirectory: './coverage',
-  extensionsToTreatAsEsm: ['.ts'],
-  testMatch: ['**/__tests__/**/*.test.*'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  automock: false,
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
-  transform: {
-    '\\.ts$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
-  },
-  passWithNoTests: true, // TODO: remove this after adding tests
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.(spec|test).[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '__tests__/mocks/*.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/build', '<rootDir>/dist', '<rootDir>/.next'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/**/index', '!src/**/types'],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config);
