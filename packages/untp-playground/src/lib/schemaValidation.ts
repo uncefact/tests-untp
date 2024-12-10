356bfad8e835de7071efdb554a1bdc4a2f10f2ac
Nam Hoang <hoangxuannam160493@gmail.com>
2024-12-10 10:18:52 +0700

    feat: support extension and multiple version of the schema

--- packages/untp-playground/src/lib/schemaValidation.ts
+++ packages/untp-playground/src/lib/schemaValidation.ts
@@ -1,5 +1,6 @@
 import addFormats from 'ajv-formats';
 import Ajv2020 from 'ajv/dist/2020';
+import { detectCredentialType, detectVersion } from './credentialService';
 
 const ajv = new Ajv2020({
   allErrors: true,
@@ -18,18 +19,100 @@ const SCHEMA_URLS = {
   DigitalIdentityAnchor: 'https://test.uncefact.org/vocabulary/untp/dia/untp-dia-schema-0.2.1.json',
 };
 
+const EXTENSION_VERSIONS: Record<
+  string,
+  { domain: string; versions: { version: string; core: { type: string; version: string } }[] }
+> = {
+  DigitalLivestockPassport: {
+    domain: 'aatp.foodagility.com',
+    versions: [
+      {
+        version: '0.4.0',
+        core: { type: 'DigitalProductPassport', version: '0.5.0' },
+      },
+    ],
+  },
+};
+
+const schemaURLConstructor = (type: string, version: string) => {
+  const shortCredentialTypes: Record<string, string> = {
+    DigitalProductPassport: 'dpp',
+    DigitalConformityCredential: 'dcc',
+    DigitalTraceabilityEvent: 'dte',
+    DigitalFacilityRecord: 'dfr',
+    DigitalIdentityAnchor: 'dia',
+  };
+  return `https://test.uncefact.org/vocabulary/untp/${shortCredentialTypes[type]}/untp-${shortCredentialTypes[type]}-schema-${version}.json`;
+};
+
+const extensionSchemaURLConstructor = (type: string, version: string) => {
+  const shortCredentialTypes: Record<string, string> = {
+    DigitalLivestockPassport: 'dlp',
+  };
+  if (type === 'DigitalLivestockPassport' && version === '0.4.0') {
+    return 'https://aatp.foodagility.com/assets/files/aatp-dlp-schema-0.4.0-9c0ad2b1ca6a9e497dedcfd8b87f35f1.json';
+  }
+  return `https://aatp.foodagility.com/vocabulary/aatp/${shortCredentialTypes[type]}/aatp-${shortCredentialTypes[type]}-schema-${version}.json`;
+};
+
 export async function validateCredentialSchema(credential: any): Promise<{
   valid: boolean;
   errors?: any[];
 }> {
-  try {
-    const credentialType = credential.type.find((t: string) => Object.keys(SCHEMA_URLS).includes(t));
+  const extension = detectExtension(credential);
+  const credentialType = extension ? extension.core.type : detectCredentialType(credential);
 
-    if (!credentialType) {
-      throw new Error('Unsupported credential type');
+  if (credentialType === 'Unknown') {
+    throw new Error('Unsupported credential type');
+  }
+
+  const version = extension?.core?.version || detectVersion(credential);
+
+  if (!version) {
+    throw new Error('Unsupported version');
+  }
+
+  const schemaUrl = schemaURLConstructor(credentialType, version);
+
+  return validateCredentialOnSchemaUrl(credential, schemaUrl, ['type', '@context']);
+}
+
+export async function validateExtension(credential: any): Promise<{
+  valid: boolean;
+  errors?: any[];
+}> {
+  const extension = detectExtension(credential);
+  if (!extension) {
+    throw new Error('Unknown extension');
+  }
+
+  const schemaUrl = extensionSchemaURLConstructor(extension.extension.type, extension.extension.version);
+
+  return validateCredentialOnSchemaUrl(credential, schemaUrl);
+}
+
+export function detectExtension(credential: any):
+  | {
+      core: { type: string; version: string };
+      extension: { type: string; version: string };
     }
+  | undefined {
+  const credentialType = detectCredentialType(credential);
+  const extension = EXTENSION_VERSIONS[credentialType];
+  if (!extension) {
+    return undefined;
+  }
+  const version = detectVersion(credential, extension.domain);
+  const extensionVersion = extension.versions.find((v) => v.version === version);
+  if (!extensionVersion) {
+    return undefined;
+  }
 
-    const schemaUrl = SCHEMA_URLS[credentialType as keyof typeof SCHEMA_URLS];
+  return {
+    core: extensionVersion.core,
+    extension: { type: credentialType, version },
+  };
+}
 
     if (!schemaCache.has(schemaUrl)) {
       const proxyUrl = `/untp-playground/api/schema?url=${encodeURIComponent(schemaUrl)}`;
