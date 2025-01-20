dd07f863cbbbf7f9f7e8b86430b73a20b7e9c0f1
Yen Le <ldhyen99@gmail.com>
2025-01-20 18:20:50 +0700

    test: implement e2e test for DIA

---
+++ cypress/e2e/issue_workflow_test/DIA/issueDIA.cy.ts
@@ -0,0 +1,110 @@
+describe('Issue DIA end-to-end testing flow', () => {
+  beforeEach(() => {
+    // Load app config JSON
+    cy.fixture('app-config.json').then((data) => {
+      Cypress.env('AppConfig', data);
+    });
+  });
+
+  it('should access the right app config data', () => {
+    const AppConfig = Cypress.env('AppConfig');
+    expect(AppConfig?.name).to.not.be.null;
+    expect(AppConfig?.name).to.not.be.undefined;
+  });
+
+  it('should visit the homepage, navigate to "Generate DIA", handle API calls, and show success message', () => {
+    const AppConfig = Cypress.env('AppConfig');
+    cy.visit('/');
+
+    cy.contains('a', 'General features').should('be.visible').and('not.be.disabled').click();
+
+    cy.url().should('include', '/general-features');
+    const feature = AppConfig.generalFeatures[0]?.features.find(
+      (feature: { name: string }) => feature.name === 'Generate DIA',
+    );
+
+    const shemaDIA = feature?.components[0].props?.schema?.url ?? '';
+    const appService = feature?.services[0]?.parameters[0] ?? {};
+    cy.intercept('GET', shemaDIA).as('getIdentityAnchor');
+
+    cy.contains('a', 'Generate DIA').should('be.visible').and('not.be.disabled').click();
+
+    // Wait for the API call
+    cy.wait('@getIdentityAnchor', { timeout: 20000 }).then((interception) => {
+      expect(interception?.response?.statusCode).to.eq(200);
+    });
+
+    const API_ENDPOINT = {
+      ISSUE_BITSTRING_STATUS_LIST: '/agent/issueBitstringStatusList',
+      VCKit_URL: appService?.vckit?.vckitAPIUrl + '/credentials/issue',
+      STORAGE_URL: appService?.storage?.url,
+      IDR_URL: appService?.dlr?.dlrAPIUrl + appService?.dlr?.linkRegisterPath,
+    };
+    cy.intercept('POST', API_ENDPOINT.ISSUE_BITSTRING_STATUS_LIST).as('issueBitStringStatusList');
+    cy.intercept('POST', API_ENDPOINT.VCKit_URL).as('issueCredentials');
+    cy.intercept('POST', API_ENDPOINT.STORAGE_URL).as('storeVC');
+    cy.intercept('POST', API_ENDPOINT.IDR_URL).as('linkResolverRegister');
+
+    cy.contains('button', 'Submit').should('be.visible').and('not.be.disabled').click();
+
+    cy.wait('@issueBitStringStatusList').then((interception) => {
+      expect(interception?.response?.statusCode).to.eq(200);
+      cy.log('Completed: issueBitstringStatusList');
+    });
+
+    cy.wait('@issueCredentials').then((interception) => {
+      const credential = interception.request.body.credential;
+      cy.log('interception: request: ', JSON.stringify(credential));
+      expect(interception?.response?.statusCode).to.eq(201);
+      cy.task('writeToFile', { fileName: 'DigitalIdentityAnchor_instance-v0.2.1.json', data: credential });
+      cy.log('Completed: issueCredentials and written to file');
+    });
+
+    // await API storage VC
+    cy.wait('@storeVC').then((interception) => {
+      expect(interception?.response?.statusCode).to.eq(201);
+      cy.log('Completed: storeVC');
+
+      // Extract the URI from the response
+      const { uri, hash, key } = interception?.response?.body;
+      expect(uri).to.not.be.undefined;
+      expect(hash).to.not.be.undefined;
+      cy.request('GET', uri).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+
+    // await API register link
+    cy.wait('@linkResolverRegister').then((interception) => {
+      expect(interception?.response?.statusCode).to.eq(201);
+      cy.log('Completed: linkResolverRegister');
+    });
+
+    // Verify toast appears, using react-toastify
+    cy.get('.Toastify__toast').should('be.visible').and('contain', 'Action Successful');
+  });
+
+  it('Verify linkType', () => {
+    const checkLinkTypeURL = 'http://localhost:3000/gs1/01/09359502000010';
+    cy.request('GET', checkLinkTypeURL).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+
+  it('Runs testing UNTP V0.2.1', () => {
+    cy.exec('pwd').then((result) => {
+      cy.log('Current directory:', result.stdout);
+    });
+    cy.task('runShellScript', { scriptPath: './cypress/e2e/issue_workflow_test/DIA/test-untp-dia-scripts.sh' }).then(
+      (output: any) => {
+        const cleanedOutput = output.replace(/\x1b\[[0-9;]*m/g, '');
+        cy.log('Shell Script Output:', cleanedOutput);
+        // Expect the output to include success message
+        expect(cleanedOutput).to.include('Script completed successfully!');
+        expect(cleanedOutput).to.include('Testing Credential: digitalIdentityAnchor');
+        // render method warning
+        expect(cleanedOutput).to.include('Result: WARN');
+      },
+    );
+  });
+});
