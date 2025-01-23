6806c73c2ecc621da819894d3a34c7b91de1b452
Huy Nguyen <huy.nguyenkim.h@gmail.com>
2025-01-23 15:13:16 +0700

    feat: implement findFirstMatchingKeyValue utility function

--- packages/services/src/features/index.ts
+++ packages/services/src/features/index.ts
@@ -1,2 +1,3 @@
 export { constructorEntryData } from './constructorEntryData.service.js';
 export { saveToLocalStorage, mergeToLocalStorage, getValueFromLocalStorage } from './localStorage.service.js';
+export { findFirstMatchingKeyValue } from './findFirstMatchingKeyValue.service.js';
