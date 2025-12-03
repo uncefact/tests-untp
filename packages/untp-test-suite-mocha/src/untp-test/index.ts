4224211076106927c643be21113eedae19fa76a3
Evstifeev Roman <someuniquename@gmail.com>
2025-12-03 15:28:08 +0500

    fix type exports

--- packages/untp-test-suite-mocha/src/untp-test/index.ts
+++ packages/untp-test-suite-mocha/src/untp-test/index.ts
@@ -5,8 +5,10 @@
  */
 
 export * from './types';
-export { UNTPTestRunner, UNTPTestResults } from './validator';
-export { StreamReporter, StreamEvent } from './stream-reporter';
+export { UNTPTestRunner } from './validator';
+export type { UNTPTestResults } from './validator';
+export { StreamReporter } from './stream-reporter';
+export type { StreamEvent } from './stream-reporter';
 export { setCredentialData, hasCredentials, getAllCredentials } from './credential-state';
 export {
   setupUNTPTests,
