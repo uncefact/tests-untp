3fd5a02b1a7e05f8c78739aa49b8c1cfa1d1aced
bla-ce <140779436+bla-ce@users.noreply.github.com>
2025-12-17 12:39:44 +0100

    chore: export interfaces and types

--- packages/services/src/interfaces/index.ts
+++ packages/services/src/interfaces/index.ts
@@ -1,1 +1,3 @@
 export * from './verifiableCredentialService.js';
+export * from './identityResolverService.js';
+// export * from './storageService.js'; TODO: uncomment once storageService has a concrete implementation replacing what's inside service.storage.ts
