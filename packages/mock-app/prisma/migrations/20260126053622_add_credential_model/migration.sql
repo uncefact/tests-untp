abb1037620ebd77a19ebb11b4bcfc9bab966c0b2
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-01-26 06:38:23 +0100

    add credential entity

---
+++ packages/mock-app/prisma/migrations/20260126053622_add_credential_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Credential" (
+    "id" TEXT NOT NULL,
+    "storageUri" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "credentialType" TEXT NOT NULL,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Credential_pkey" PRIMARY KEY ("id")
+);
