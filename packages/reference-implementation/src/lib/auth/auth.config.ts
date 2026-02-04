631d8cb6ab5386eddc45be63b145ece5be922270
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-02-04 13:24:23 +0100

    fix: UntrustedHost error in Docker container build

--- packages/reference-implementation/src/lib/auth/auth.config.ts
+++ packages/reference-implementation/src/lib/auth/auth.config.ts
@@ -10,6 +10,7 @@ import { type NextAuthConfig } from 'next-auth';
 import Keycloak from 'next-auth/providers/keycloak';
 
 export const authConfig: NextAuthConfig = {
+  trustHost: true,
   session: {
     strategy: 'jwt',
     maxAge: 8 * 60 * 60,
