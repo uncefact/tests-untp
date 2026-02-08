77d0d0ff78cd368d1d73966d849ed21c969f2aad
ashleythedeveloper <ashley.harwood.au@gmail.com>
2026-02-09 08:01:48 +1100

    fix(components): add moduleNameMapper for renamed services package
    Jest in the components package runs via lerna exec within the package
    directory. The jest-resolve module fails to traverse up to the hoisted
    node_modules to find @uncefact/untp-ri-services. Map it directly to
    the workspace source.

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -16,6 +16,7 @@ const jestConfig = {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^react($|/.+)': '<rootDir>/../../node_modules/react$1',
     '^react-dom($|/.+)': '<rootDir>/../../node_modules/react-dom$1',
+    '^@uncefact/untp-ri-services$': '<rootDir>/../services/src/index.ts',
   },
   transform: {
     '^.+\\.m?tsx?$': [
