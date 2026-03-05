fa4bd6fe9c0ebfa51cecf35fd077100129e0d758
Ashley Harwood <ashley.harwood.au@gmail.com>
2026-03-06 08:13:46 +1100

    fix(storage): parameterise hardcoded storage bucket names (#452)

--- packages/reference-implementation/prisma/seed.ts
+++ packages/reference-implementation/prisma/seed.ts
@@ -288,8 +288,8 @@ async function main() {
         baseUrl: new URL(storageServiceUrl).origin,
         ...(storageApiKey && { apiKey: storageApiKey }),
         apiVersion: '3.0.0',
-        publicBucket: 'public-data',
-        privateBucket: 'private-data',
+        publicBucket: process.env.UNCEFACT_STORAGE_PUBLIC_BUCKET || 'public-data',
+        privateBucket: process.env.UNCEFACT_STORAGE_PRIVATE_BUCKET || 'private-data',
       });
       const encryptedStorageConfig = JSON.stringify(
         encryptionService.encrypt(storageServiceConfig, EncryptionAlgorithm.AES_256_GCM),
@@ -449,7 +449,7 @@ async function main() {
       const { storageServiceUrl } = getStorageConfig();
       const storageApiKey = process.env.UNCEFACT_STORAGE_API_KEY;
       const storageBaseUrl = new URL(storageServiceUrl).origin;
-      const storageBucket = 'public-data';
+      const storageBucket = process.env.UNCEFACT_STORAGE_PUBLIC_BUCKET || 'public-data';
 
       const TEMPLATES_DIR = path.resolve(__dirname, '../src/templates/v0.6.0');
 
