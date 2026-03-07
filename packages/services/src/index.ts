7456a6a41a33f7800bdaa4dc812049e056432676
ashleythedeveloper <ashley.harwood.au@gmail.com>
2026-03-08 10:58:30 +1100

    chore(services): update barrel exports and verify build
    Remove re-exports of deleted legacy modules (epcisEvents, processDPP,
    processDigitalIdentityAnchor, processDigitalFacilityRecord,
    processDigitalConformityCredential, vckit.service, linkResolver.service,
    storage.service, identityProviders, features, api.service,
    identifierSchemes) from the services package barrel file.

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -1,17 +1,5 @@
 export * from './types/index.js';
-export * from './epcisEvents/index.js';
-export * from './processDPP.service.js';
-export * from './processDigitalIdentityAnchor.service.js';
-export * from './processDigitalFacilityRecord.service.js';
-export * from './processDigitalConformityCredential.service.js';
-export * from './vckit.service.js';
-export * from './linkResolver.service.js';
-export * from './storage.service.js';
 export * from './utils/index.js';
-export * from './identityProviders/index.js';
-export * from './features/index.js';
-export * from './api.service.js';
-export * from './identifierSchemes/index.js';
 export type {
   CredentialPayload,
   CredentialIssuer,
