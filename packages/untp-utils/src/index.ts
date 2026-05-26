868bc04bbfa434b55b0cb38ee2ed16eb94066c23
ashleythedeveloper <ashley.harwood.au@gmail.com>
2026-05-26 14:08:10 +1000

    feat(utils): add StructuredError base class for ADR-035 rollout

--- packages/untp-utils/src/index.ts
+++ packages/untp-utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from './multibase-digest/index.js';
+export * from './structured-error.js';
 export * from './validation-outcome.js';
 export * from './detect-version-from-context.js';
 export * from './conformity-vocabulary/index.js';
